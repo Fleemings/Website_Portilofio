d48524d20b1d4a2a85f19dc6fa7b3046f6a9778f
Milena Fleming <milenasapucaia29@gmail.com>
2023-03-05 18:30:53 +0000

    feat(footer): created the footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
 import './footer.css';
+import { RiLinkedinFill } from 'react-icons/ri';
+import { BsGithub, BsInstagram } from 'react-icons/bs';
 
 const footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <a href='#' className='footer__logo'>
+        Milena Fleming
+      </a>
+      <ul className='footer__links'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#skill'>Experience</a>
+        </li>
+        <li>
+          <a href='#project'>Portifolio</a>
+        </li>
+      </ul>
+      <div className='footer__socials'>
+        <a
+          href='https://www.linkedin.com/in/milenafleming/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <RiLinkedinFill />
+        </a>
+        <a
+          href='https://github.com/Fleemings'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <BsGithub />
+        </a>
+        <a
+          href='https://www.instagram.com/fleemings/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <BsInstagram />
+        </a>
+      </div>
+      <div className='footer__final'>
+        <small>Thank you for passing by. </small>
+      </div>
+    </footer>
+  );
 };
 
 export default footer;

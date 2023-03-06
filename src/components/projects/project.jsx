285f8244d2942cecbe1368adaa6cfc597d788ada
Milena Fleming <milenasapucaia29@gmail.com>
2023-03-06 00:26:24 +0000

    feat(project): project component cards and data created

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import './project.css';
+import data from './projectData';
 
 function project() {
-  return <section id='project'>project</section>;
+  return (
+    <section id='project'>
+      <h5>view the archive</h5>
+      <h2>Other Noteworthy Projects</h2>
+
+      <div className='container project__container'>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className='project__item'>
+              <div className='project__item-image'>
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='project__item-cta'>
+                <a href={github} className='btn' target='_blank'>
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className='btn btn-primary'
+                  target='_blank'
+                >
+                  Live
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default project;
---
+++ src/components/projects/projectData.js
@@ -0,0 +1,50 @@
+import ImageSoon from '../../assets/images/coming-soon.jpg';
+import FindWorkImage from '../../assets/images/find-work.jpg';
+import TinderImage from '../../assets/images/tinder.jpg';
+
+const data = [
+  {
+    id: 1,
+    image: FindWorkImage,
+    title: 'Find Work - Laravel',
+    github: 'https://github.com/Fleemings/findWork',
+    demo: 'https://github.com/Fleemings/findWork',
+  },
+  {
+    id: 2,
+    image: TinderImage,
+    title: 'Tinder - React, MongoDB',
+    github: 'https://github.com/Fleemings/Tinder',
+    demo: '',
+  },
+  {
+    id: 3,
+    image: ImageSoon,
+    title: 'Link Shorter',
+    github: '',
+    demo: '',
+  },
+  {
+    id: 4,
+    image: ImageSoon,
+    title: 'Spotfy Me version',
+    github: '',
+    demo: '',
+  },
+  {
+    id: 5,
+    image: ImageSoon,
+    title: 'Wheather App',
+    github: '',
+    demo: '',
+  },
+  {
+    id: 6,
+    image: ImageSoon,
+    title: 'Movies Catalog',
+    github: '',
+    demo: '',
+  },
+];
+
+export default data;
